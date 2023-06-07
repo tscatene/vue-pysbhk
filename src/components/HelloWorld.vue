3f87c9c463dac8ae05c03255e85a864420076155
tscatene <135832498+tscatene@users.noreply.github.com>
2023-06-07 14:02:41 +0200

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="hello-world">
+    <link
+      v-for="(cssUrl, index) in cssUrls"
+      :key="index"
+      :href="cssUrl"
+      rel="stylesheet"
+    />
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: {
+      type: String,
+      default: 'Hello World!',
+    },
+    cssUrls: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  mounted() {
+    this.cssUrls.forEach((cssUrl) => {
+      let link = document.createElement('link');
+      link.rel = 'stylesheet';
+      link.href = cssUrl;
+      this.$el.appendChild(link);
+    });
+  },
+};
+</script>
